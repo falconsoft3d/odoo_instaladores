32c0eec48cf5518b6069ce54987f8f15304914cb
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-05-21 16:00:32 -0400

    1
    1

---
+++ libredte_git_pull.sh
@@ -0,0 +1,10 @@
+cd /opt/odoo/extra-addons/odoo_chile_community
+git pull
+
+cd /opt/odoo/extra-addons/l10n_cl_facturae_libredte
+git pull
+
+cd /opt/odoo/extra-addons/l10n_cl_payroll
+git pull
+
+echo "Terminado"
