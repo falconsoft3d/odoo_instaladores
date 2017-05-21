1296a26e9939c044024c29727c5464e97cb30b32
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-05-21 16:08:51 -0400

    1
    1

--- webfactura_git_clone.sh
+++ webfactura_git_clone.sh
@@ -1,8 +1,5 @@
 echo "Clonamos los repositorios"
-git clone https://github.com/falconsoft3d/odoo-general
-git clone -b 10.0 https://github.com/falconsoft3d/odoo_chile_community
 git clone https://github.com/falconsoft3d/l10n_cl_facturae_webfactura
-git clone https://github.com/falconsoft3d/l10n_cl_payroll
 
 echo "Ponemos el usuario de Odoo con permiso"
 chown odoo: /opt/odoo/ -R
