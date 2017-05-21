def8dcc75169a1723fd4da9a6091641912b1c231
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-05-21 16:07:57 -0400

    1
    1

--- git_clone_chile.sh
+++ git_clone_chile.sh
@@ -1,7 +1,6 @@
 echo "Clonamos los repositorios"
 git clone https://github.com/falconsoft3d/odoo-general
 git clone -b 10.0 https://github.com/falconsoft3d/odoo_chile_community
-git clone https://github.com/falconsoft3d/l10n_cl_facturae_libredte
 git clone https://github.com/falconsoft3d/l10n_cl_payroll
 
 echo "Ponemos el usuario de Odoo con permiso"
