3106e43117a3d584e8992152a29dc776c492e108
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-05-21 16:14:21 -0400

    1
    1

---
+++ git_pull_chile.sh
@@ -0,0 +1,10 @@
+cd /opt/odoo/extra-addons/odoo-general
+git pull
+
+cd /opt/odoo/extra-addons/odoo_chile_community
+git pull
+
+cd /opt/odoo/extra-addons/l10n_cl_payroll
+git pull
+
+echo "Terminado"
