f36b856108bb42c05383aa9bbdce930f12f25d51
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-05-21 15:41:30 -0400

    1
    1

--- libredte_git_clone.sh
+++ libredte_git_clone.sh
@@ -1,12 +1,12 @@
 echo "Creamos las carpetas"
-mkdir /opt/odoo/server/extra-addons/odoo_chile_community
-mkdir /opt/odoo/server/extra-addons/l10n_cl_facturae_libredte
-mkdir /opt/odoo/server/extra-addons/l10n_cl_payroll
+mkdir /opt/odoo/extra-addons/odoo_chile_community
+mkdir /opt/odoo/extra-addons/l10n_cl_facturae_libredte
+mkdir /opt/odoo/extra-addons/l10n_cl_payroll
 
 echo "Clonamos los repositorios"
-git clone -b 10.0 https://github.com/falconsoft3d/odoo_chile_community /opt/odoo/server/extra-addons/odoo_chile_community
-git clone https://github.com/falconsoft3d/l10n_cl_facturae_libredte /opt/odoo/server/extra-addons/l10n_cl_facturae_libredte
-git clone https://github.com/falconsoft3d/l10n_cl_payroll /opt/odoo/server/extra-addons/l10n_cl_payroll
+git clone -b 10.0 https://github.com/falconsoft3d/odoo_chile_community /opt/odoo/extra-addons/odoo_chile_community
+git clone https://github.com/falconsoft3d/l10n_cl_facturae_libredte /opt/odoo/extra-addons/l10n_cl_facturae_libredte
+git clone https://github.com/falconsoft3d/l10n_cl_payroll /opt/odoo/extra-addons/l10n_cl_payroll
 
 echo "Ponemos el usuario de Odoo con permiso"
 chown odoo: /opt/odoo/ -R
